b3a78dd7b05b77f5d3ffcb61d39ea8a99ab846c9
Andrés Osorio López <andresosoriolopez@gmail.com>
2022-01-16 16:32:12 -0300

    Add pom and main class

---
+++ src/main/java/cl/andres/streams/j8/StreamsJ8.java
@@ -0,0 +1,11 @@
+package cl.andres.streams.j8;
+
+public class StreamsJ8 {
+    /**
+     *
+     * @param args in arguments
+     */
+    public static void main(String[] args)  {
+        System.out.println("Hey!");
+    }
+}
